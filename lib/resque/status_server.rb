fc3fe3680c98569d444d60201dcd945d23d292fb
Aaron Quint <aaron@quirkey.com>
2010-01-16 12:18:18 -0500

    Working, but no actual help, server plugin

--- lib/resque/status_server.rb
+++ lib/resque/status_server.rb
@@ -1,15 +1,25 @@
 require 'resque/status'
 
 module Resque
-  class StatusServer
+  module StatusServer
+    
+    VIEW_PATH = File.join(File.dirname(__FILE__), 'server', 'views')
     
     def self.registered(app)
       
+      
       app.get '/statuses' do
         @statuses = Resque::Status.statuses
-        erb File.join(File.dirname(__FILE__), 'server', 'views', 'statuses.erb')
+        status_view(:statuses)
       end
       
+      app.helpers do
+        def status_view(filename)
+          erb(File.read(File.join(VIEW_PATH, "#{filename}.erb")))
+        end
+      end
+      
+      app.tabs << "Statuses"
     end
 
   end
