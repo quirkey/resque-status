77f855da7b71a22078ef52ab44480d27cff12d8c
Aaron Quint <aaron@quirkey.com>
2010-01-20 14:52:44 -0500

    Always merge the last status with the newest

--- lib/resque/job_with_status.rb
+++ lib/resque/job_with_status.rb
@@ -180,7 +180,7 @@ module Resque
     
     private
     def set_status(*args)
-      self.status = [{'name'  => self.name}, args].flatten
+      self.status = [status, {'name'  => self.name}, args].flatten
     end
     
   end
