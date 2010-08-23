ae392c6d421d02db528637adc084da5c24e4550d
Aaron Quint <aaron@quirkey.com>
2010-08-23 09:41:10 -0700

    Version bump

--- lib/resque/status.rb
+++ lib/resque/status.rb
@@ -7,7 +7,7 @@ module Resque
   # the common status attributes. It also has a number of class methods for 
   # creating/updating/retrieving status objects from Redis
   class Status < Hash
-    VERSION = '0.1.4'
+    VERSION = '0.1.5'
 
     extend Resque::Helpers
 
