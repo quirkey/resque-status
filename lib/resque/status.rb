29062cfca8a94d1f49579405aa3c88522b09236e
humancopy <info@humancopy.net>
2011-08-12 14:25:50 +0200

    Add time zone to status time display

--- lib/resque/status.rb
+++ lib/resque/status.rb
@@ -205,7 +205,7 @@ module Resque
     # Return the time of the status initialization. If set returns a <tt>Time</tt>
     # object, otherwise returns nil
     def time
-      time? ? Time.at(self['time']).strftime("%Y/%m/%d %H:%M:%S") : nil
+      time? ? Time.at(self['time']).strftime("%Y/%m/%d %H:%M:%S %Z") : nil
     end
 
     STATUSES.each do |status|
