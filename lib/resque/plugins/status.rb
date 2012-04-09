adec2183f237274bd06cb2fd5dfe3cee649766ad
Aaron Quint <aaron@quirkey.com>
2012-04-08 23:30:16 -0700

    Version bump

--- lib/resque/plugins/status.rb
+++ lib/resque/plugins/status.rb
@@ -28,7 +28,7 @@ module Resque
     # This job would iterate num times updating the status as it goes. At the end
     # we update the status telling anyone listening to this job that its complete.
     module Status
-      VERSION = '0.3.2'
+      VERSION = '0.3.3'
 
       autoload :Hash, 'resque/plugins/status/hash'
 
